7bc4727176d183097cf980df1dd450fabc8224d7
Nadukula_Hemanth <23eg107e37@anurag.edu.in>
2026-01-23 19:47:34 +0530

    Student Submission: Admin - javascript #1
    Language: javascript
    Student: Admin (admin@aisla.com)
    Submission: #1
    File: sum of two 
    Time: 2026-01-23T14:17:34.505Z
    
    Problem: sum of two numbers

---
+++ students/admin/javascript/sum_of_two_.js
@@ -0,0 +1,21 @@
+/*
+ * ========================================
+ * AISLA - Student Code Submission
+ * ========================================
+ * Student: Admin
+ * Email: admin@aisla.com
+ * Submission: #1
+ * Date: 23/1/2026, 7:47:34 pm
+ * Language: javascript
+ * Problem: sum of two numbers...
+ * ========================================
+ */
+
+// JavaScript Code
+// Start coding here!
+
+function main() {
+    console.log("Hello, World!");
+}
+
+main();
