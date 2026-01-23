f3ad723707122164669aeff011a1577c624c6e5d
Nadukula_Hemanth <23eg107e37@anurag.edu.in>
2026-01-23 15:53:58 +0530

    Student Submission: Admin - python #5
    Language: python
    Student: Admin (admin@aisla.com)
    Submission: #5
    File: shash
    Time: 2026-01-23T10:23:57.369Z

---
+++ students/admin/python/shash.py
@@ -0,0 +1,11 @@
+# ========================================
+# AISLA - Student Code Submission
+# ========================================
+# Student: Admin
+# Email: admin@aisla.com
+# Submission: #5
+# Date: 23/1/2026, 3:53:57 pm
+# Language: python
+# ========================================
+
+print("sheshank in yehlanka")
