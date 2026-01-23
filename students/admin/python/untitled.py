cdc1de425d4065e7c0d75bf7b56f48bf91c15b94
Nadukula_Hemanth <23eg107e37@anurag.edu.in>
2026-01-23 15:51:59 +0530

    Student Submission: Admin - python #4
    Language: python
    Student: Admin (admin@aisla.com)
    Submission: #4
    File: untitled
    Time: 2026-01-23T10:21:58.003Z

---
+++ students/admin/python/untitled.py
@@ -0,0 +1,11 @@
+# ========================================
+# AISLA - Student Code Submission
+# ========================================
+# Student: Admin
+# Email: admin@aisla.com
+# Submission: #4
+# Date: 23/1/2026, 3:51:58 pm
+# Language: python
+# ========================================
+
+print("sheshank")
